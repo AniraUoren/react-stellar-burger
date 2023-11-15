7b798fad599e0c21da6e238558d0aa685ac4437a
Anira Uoren <airin6666@yandex.ru>
2023-11-15 16:43:25 +0300

    feat: AppHeader
    Собран заголовок
    
    Closes WP-226 Step 1 - AppHeader

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Styles from "./app-header.module.css"
+import { Logo, Button, BurgerIcon, ListIcon, ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components"
+
+function AppHeader() {
+    return(
+        <header className={Styles.header}>
+            <div className={Styles.container}>
+                <div>
+                    <Button htmlType="button" type="secondary" size="medium" extraClass="pl-5 pr-5 mr-2" style={{color: "#fff"}}>
+                        <BurgerIcon type="primary"/> <p className="text text_type_main-default">Конструктор</p>
+                    </Button>
+                    <Button htmlType="button" type="secondary" size="medium" extraClass="pl-5 pr-5">
+                        <ListIcon type="secondary" /> <p className="text text_type_main-default text_color_inactive">Лента заказов</p>
+                    </Button>
+                </div>
+                <Logo className="mr-30"/>
+                <Button htmlType="button" type="secondary" size="medium" extraClass="pl-5 pr-5">
+                    <ProfileIcon type="secondary" /> <p className="text text_type_main-default text_color_inactive">Личный кабинет</p>
+                </Button>
+            </div>
+        </header>
+    )
+}
+
+export default AppHeader;
