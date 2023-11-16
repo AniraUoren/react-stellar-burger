6a1cf186bbd6ce3e855927461544991b407e925a
Anira Uoren <airin6666@yandex.ru>
2023-11-16 15:27:21 +0300

    feat: AppHeader
    Поправлены стили заголовка
    
    Closes WP-226 Step 1 - AppHeader

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import Styles from "./app-header.module.css"
-import { Logo, Button, BurgerIcon, ListIcon, ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components"
+import {Logo, Button, BurgerIcon, ListIcon, ProfileIcon} from "@ya.praktikum/react-developer-burger-ui-components"
 
 function AppHeader() {
-    return(
+    return (
         <header className={Styles.header}>
             <div className={Styles.container}>
-                <div>
-                    <Button htmlType="button" type="secondary" size="medium" extraClass="pl-5 pr-5 mr-2" style={{color: "#fff"}}>
-                        <BurgerIcon type="primary"/> <p className="text text_type_main-default">Конструктор</p>
+                <div style={{display: "flex", flexDirection: "row"}}>
+                    <Button htmlType="button" type="secondary" size="medium" extraClass={`${Styles.button} pl-5 pr-5 mr-2`} style={{color: "#fff"}}>
+                        <BurgerIcon type="primary"/> <p className={`${Styles.btnText} text text_type_main-default`}>Конструктор</p>
                     </Button>
-                    <Button htmlType="button" type="secondary" size="medium" extraClass="pl-5 pr-5">
-                        <ListIcon type="secondary" /> <p className="text text_type_main-default text_color_inactive">Лента заказов</p>
+                    <Button htmlType="button" type="secondary" size="medium" extraClass={`${Styles.button} pl-5 pr-5`}>
+                        <ListIcon type="secondary" /> <p className={`${Styles.btnText} text text_type_main-default text_color_inactive`}>Лента заказов</p>
                     </Button>
                 </div>
                 <Logo className="mr-30"/>
-                <Button htmlType="button" type="secondary" size="medium" extraClass="pl-5 pr-5">
-                    <ProfileIcon type="secondary" /> <p className="text text_type_main-default text_color_inactive">Личный кабинет</p>
+                <Button htmlType="button" type="secondary" size="medium" extraClass={`${Styles.button} pl-5 pr-5`}>
+                    <ProfileIcon type="secondary"/> <p
+                    className={`${Styles.btnText} text text_type_main-default text_color_inactive`}>Личный кабинет</p>
                 </Button>
             </div>
         </header>
