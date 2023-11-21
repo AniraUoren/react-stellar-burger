a78097c0509eaae4bd7cf1d276787ab2a2ed2044
Anira Uoren <airin6666@yandex.ru>
2023-11-21 17:28:21 +0300

    fix: Поправлены стили до PP

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -55,6 +55,7 @@ function BurgerConstructor() {
             "image": "https://code.s3.yandex.net/react/code/mineral_rings.png"
         }
     ]);
+    const [price, setPrice] = React.useState(610);
     return (
         <div className={`${Styles.block} pt-25`}>
             <div className={`${Styles.container} mb-10`}>
@@ -79,7 +80,7 @@ function BurgerConstructor() {
                                         text={elem.name}
                                         price={elem.price}
                                         thumbnail={elem.image}
-                                        extraClass="ml-1"
+                                        extraClass="ml-1 mr-2"
                                     />
                                 </div>
                             )
@@ -93,16 +94,16 @@ function BurgerConstructor() {
                         text="Краторная булка N-200i (низ)"
                         price={200}
                         thumbnail={"https://code.s3.yandex.net/react/code/meat-01.png"}
-                        extraClass="ml-8"
+                        extraClass="ml-8 mr-2"
                     />
                 </div>
             </div>
             <div className={`${Styles.total} mt-40`}>
                 <div className={`${Styles.price}`}>
-                    <p className="text text_type_digits-medium">610</p>
+                    <p className="text text_type_digits-medium mr-2">{price}</p>
                     <CurrencyIcon type="primary"/>
                 </div>
-                <Button htmlType="button" type="primary" size="large" extraClass="ml-10">
+                <Button htmlType="button" type="primary" size="large" extraClass="ml-10 mr-4">
                     Оформить заказ
                 </Button>
             </div>
