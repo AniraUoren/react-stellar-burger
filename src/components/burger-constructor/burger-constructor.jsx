0f07f3b9e098e93766a6d1c3cf2f1eb55bc512e1
Anira Uoren <airin6666@yandex.ru>
2023-11-29 22:11:49 +0300

    fix: Исправление замечаний мидл-кодревью 1
    3. Отрисовывается правильная картинка для булочки.

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -3,6 +3,7 @@ import {Button, ConstructorElement, CurrencyIcon, DragIcon} from "@ya.praktikum/
 
 import Styles from "./burger-constructor.module.css";
 import {cartData} from "../../utils/data";
+import {element} from "prop-types";
 
 function BurgerConstructor() {
     const [cart, setCart] = React.useState(cartData);
@@ -11,54 +12,68 @@ function BurgerConstructor() {
         <div className={`${Styles.block} pt-25`}>
             <div className={`${Styles.container} mb-10`}>
                 <div>
-                    <ConstructorElement
-                        type="top"
-                        isLocked={true}
-                        text="Краторная булка N-200i (верх)"
-                        price={200}
-                        thumbnail={"https://code.s3.yandex.net/react/code/meat-01.png"}
-                        extraClass="ml-8"
-                    />
-                </div>
-                <div className={`${Styles.center} custom-scroll`}>
                     {cart.map((elem, index) => {
-                        if (elem.type !== "bun") {
+                        if (elem.type === "bun") {
                             return (
-                                <div key={`${elem._id}${index}`}>
-                                    <DragIcon type="primary"/>
-                                    <ConstructorElement
-                                        text={elem.name}
-                                        price={elem.price}
-                                        thumbnail={elem.image}
-                                        extraClass="ml-1 mr-2"
-                                    />
-                                </div>
+                                <ConstructorElement
+                                    type="top"
+                                    isLocked={true}
+                                    text={elem.name}
+                                    price={elem.price}
+                                    thumbnail={elem.image}
+                                    extraClass="ml-8"
+                                    key={`${elem._id}${index}`}
+                                />
                             )
                         }
                     })}
-                </div>
-                <div>
-                    <ConstructorElement
-                        type="bottom"
-                        isLocked={true}
-                        text="Краторная булка N-200i (низ)"
-                        price={200}
-                        thumbnail={"https://code.s3.yandex.net/react/code/meat-01.png"}
-                        extraClass="ml-8 mr-2"
-                    />
-                </div>
             </div>
-            <div className={`${Styles.total} mt-40`}>
-                <div className={`${Styles.price}`}>
-                    <p className="text text_type_digits-medium mr-2">{price}</p>
-                    <CurrencyIcon type="primary"/>
-                </div>
-                <Button htmlType="button" type="primary" size="large" extraClass="ml-10 mr-4">
-                    Оформить заказ
-                </Button>
+            <div className={`${Styles.center} custom-scroll`}>
+                {cart.map((elem, index) => {
+                    if (elem.type !== "bun") {
+                        return (
+                            <div key={`${elem._id}${index}`}>
+                                <DragIcon type="primary"/>
+                                <ConstructorElement
+                                    text={elem.name}
+                                    price={elem.price}
+                                    thumbnail={elem.image}
+                                    extraClass="ml-1 mr-2"
+                                />
+                            </div>
+                        )
+                    }
+                })}
+            </div>
+            <div>
+                {cart.map((elem, index) => {
+                    if (elem.type === "bun") {
+                        return (
+                            <ConstructorElement
+                                type="bottom"
+                                isLocked={true}
+                                text={elem.name}
+                                price={elem.price}
+                                thumbnail={elem.image}
+                                extraClass="ml-8"
+                                key={`${elem._id}${index}`}
+                            />
+                        )
+                    }
+                })}
             </div>
         </div>
-    )
+    <div className={`${Styles.total} mt-40`}>
+        <div className={`${Styles.price}`}>
+            <p className="text text_type_digits-medium mr-2">{price}</p>
+            <CurrencyIcon type="primary"/>
+        </div>
+        <Button htmlType="button" type="primary" size="large" extraClass="ml-10 mr-4">
+            Оформить заказ
+        </Button>
+    </div>
+</div>
+)
 
 }
 
