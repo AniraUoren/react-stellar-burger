fa77b3ae7513c2094cb1ac28a6d9802eea7105c8
Anira Uoren <airin6666@yandex.ru>
2023-12-07 17:23:22 +0300

    WP-236 Типизация

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -4,7 +4,6 @@ import {Tab} from "@ya.praktikum/react-developer-burger-ui-components";
 import Styles from "./burger-ingredients.module.css"
 
 import BurgerIngredient from "../burger-ingredient/burger-ingredient";
-import {ingredientPropType} from "../../utils/prop-types";
 import PropTypes from "prop-types";
 
 
