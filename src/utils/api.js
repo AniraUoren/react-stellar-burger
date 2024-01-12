9921b2cc1dd2579e3b1f8a73c10f4c3c9b4863a3
Anira Uoren <airin6666@yandex.ru>
2024-01-12 21:01:45 +0300

    WP-273 Путь к API вынести в константу

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,11 @@
 import {isResOk} from "./utils";
+import {api_url} from "./consts";
 
-export const getIngredientsAPI = () => fetch(`https://norma.nomoreparties.space/api/ingredients`)
+export const getIngredientsAPI = () => fetch(`${api_url}/ingredients`)
     .then(isResOk)
     .catch(err => console.error(`Произошла ошибка: ${err}`))
 
-export const getOrderIdAPI = (data) => fetch(`https://norma.nomoreparties.space/api/orders`, {
+export const getOrderIdAPI = (data) => fetch(`${api_url}/orders`, {
     method: "POST",
     headers: {
         'Content-type': "application/json",
